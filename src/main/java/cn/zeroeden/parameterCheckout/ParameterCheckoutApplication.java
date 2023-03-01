6a5d59318f97d4753a0b446da1d428aeb39c7132
zhengYongCheng <1761430646@qq.com>
2023-03-01 20:10:16 +0800

    [启动failFast机制]

--- src/main/java/cn/zeroeden/parameterCheckout/ParameterCheckoutApplication.java
+++ src/main/java/cn/zeroeden/parameterCheckout/ParameterCheckoutApplication.java
@@ -1,13 +1,29 @@
 package cn.zeroeden.parameterCheckout;
 
+import org.hibernate.validator.HibernateValidator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+
 
 @SpringBootApplication
 public class ParameterCheckoutApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ParameterCheckoutApplication.class, args);
+    }
+
+    @Bean
+    public Validator validator(){
+        ValidatorFactory validatorFactory = Validation.byProvider(HibernateValidator.class)
+                .configure()
+                .failFast(true)
+                .buildValidatorFactory();
+        return validatorFactory.getValidator();
     }
 
 }
